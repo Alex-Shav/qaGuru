d438d86564f19a71aa1abf8f0c5c2507cb819fb2
Alex Shavlyuk <82819234+Alex-Shav@users.noreply.github.com>
2023-02-16 18:48:42 +0300

    Update SearchTests.java

--- SearchTests.java
+++ SearchTests.java
@@ -2,7 +2,9 @@ public class SearchTests {
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
+        
         $("[name=q]").setValue("selenide").pressEnter();
+        
         $("[id=search]").shouldHave(text("https://selenide.org!!!!"));
     }
 }
